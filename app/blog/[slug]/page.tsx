4b50f710ab9115f783759acc3e735ceb11e3031e
TraceCohenTech <t@nyvp.com>
2026-05-10 03:38:54 -0400

    chore: update blog/[slug]/page.tsx import to articles-extra

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Link from "next/link";
-import { articles, getArticleBySlug } from "@/app/blog/data/articles";
+import { articles, getArticleBySlug } from "@/app/blog/data/articles-extra";
 import Breadcrumbs from "@/app/components/Breadcrumbs";
 
 interface PageProps {
