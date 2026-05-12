e63cc922a852c7c4077048aba60493f1bc715d43
TraceCohenTech <t@nyvp.com>
2026-05-12 03:45:01 -0400

    Update blog/[slug]/page.tsx to import from articles-extra2

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Link from "next/link";
-import { articles, getArticleBySlug } from "@/app/blog/data/articles-extra";
+import { articles, getArticleBySlug } from "@/app/blog/data/articles-extra2";
 import Breadcrumbs from "@/app/components/Breadcrumbs";
 
 interface PageProps {
